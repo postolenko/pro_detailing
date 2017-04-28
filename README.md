6a9ff435943b23b04c000ca1cc9597ee5b16f6a9
postolenko <denis.postolenko@gmail.com>
2017-04-28 17:04:27 +0300

    Pro Datailing

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/pro_datailing/index.html
---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+
+    $(window).resize(function() {
+
+
+        // $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        // $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    });
+
+
+    // $(function() {
+
+    //     $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+    //     $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    // });
+
+
+});
